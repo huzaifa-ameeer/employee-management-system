54a6f5a51351036298c454a98a7f4ca0e54f690c
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-03-24 15:37:06 +0500

    project setupt

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.css'
+
+const App = () => {
+  return (
+    <div>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam, odio.</p>
+    </div>
+  )
+}
+
+export default App
