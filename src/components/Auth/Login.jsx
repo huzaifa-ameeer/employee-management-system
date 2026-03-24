a630d40d2343da8dc553e20f8b0bb9c02fb33a29
Muhammad Huzaifa Ameer <huzaifahnbhai098@gmail.com>
2026-03-24 19:10:56 +0500

    added form handling

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,20 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="flex items-center justify-center min-h-screen">
       <div>
-        <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-          <legend className="fieldset-legend">L O G I N</legend>
+        <form
+          onSubmit={(e) => {
+            handleSubmit(e);
+          }}
+        >
+          <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
+            <legend className="fieldset-legend">L O G I N</legend>
 
-          <label className="label">Email</label>
-          <input type="email" className="input focus:border-purple-500 focus:outline-none" placeholder="Email" />
+            <label className="label">Email</label>
+            <input
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+              required
+              type="email"
+              className="input focus:border-purple-500 focus:outline-none"
+              placeholder="Email"
+            />
 
-          <label className="label">Password</label>
-          <input type="password" className="input focus:border-purple-500 focus:outline-none" placeholder="Password" />
+            <label className="label">Password</label>
+            <input
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+              required
+              type="password"
+              className="input focus:border-purple-500 focus:outline-none"
+              placeholder="Password"
+            />
 
-          <button className="btn btn-neutral mt-4 active:border-purple-500 focus:outline-none">Login</button>
-        </fieldset>
+            <button className="btn btn-neutral mt-4 active:border-purple-500 focus:outline-none">
+              Login
+            </button>
+          </fieldset>
+        </form>
       </div>
     </div>
   );
